db6baa089671ae4b3314d9cdbbb06f2d06f40d68
Ferdinand Prantl <prantlf@gmail.com>
2021-12-12 16:25:53 +0100

    chore: Upgrade TypeScript types to the latest interface
    * Lint the types.

--- lib/nettime.d.ts
+++ lib/nettime.d.ts
@@ -1,49 +1,55 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 declare interface NettimeOptions {
-  url: string;
-  credentials?: NettimeRequestCredentials;
-  method?: string;
-  headers?: object;
-  timeout?: number;
-  rejectUnauthorized?: boolean;
-  requestCount?: number;
-  requestDelay?: number;
-  outputFile?: string;
-  returnResponse?: boolean;
-  includeHeaders?: boolean;
-  appendToOutput?: any;
-  failOnOutputFileError?: boolean;
-  httpVersion?: string;
-  data?: any;
+  url: string
+  credentials?: NettimeRequestCredentials
+  method?: string
+  headers?: object
+  timeout?: number
+  followRedirects?: boolean
+  rejectUnauthorized?: boolean
+  requestCount?: number
+  requestDelay?: number
+  outputFile?: string
+  returnResponse?: boolean
+  includeHeaders?: boolean
+  appendToOutput?: any
+  failOnOutputFileError?: boolean
+  httpVersion?: string
+  data?: any
 }
 
 declare interface NettimeRequestCredentials {
-  username: string;
-  password: string;
+  username: string
+  password: string
 }
 
 declare interface NettimeResponse {
-  timings: NettimeTimings;
-  httpVersion: string;
-  statusCode: number;
-  statusMessage: string;
-  headers?: object;
-  response?: Buffer;
+  url?: string
+  timings: NettimeTimings
+  httpVersion: string
+  statusCode: number
+  statusMessage: string
+  headers?: object
+  response?: Buffer
 }
 
 declare interface NettimeTimings {
-  socketOpen: number[];
-  dnsLookup: number[];
-  tcpConnection: number[];
-  tlsHandshake: number[];
-  firstByte: number[];
-  contentTransfer: number[];
-  socketClose: number[];
+  socketOpen: number[]
+  dnsLookup: number[]
+  tcpConnection: number[]
+  tlsHandshake: number[]
+  firstByte: number[]
+  contentTransfer: number[]
+  socketClose: number[]
 }
 
-declare function nettime(options: NettimeOptions): Promise<NettimeResponse>;
+declare function nettime(options: string | NettimeOptions): Promise<NettimeResponse>
+
 declare namespace nettime {
-  export function getDuration (start: number, end: number): number;
-  export function getMilliseconds (timings: number[]): number;
+  export function getDuration (start: number, end: number): number
+  export function getMilliseconds (timings: number[]): number
+  export function isRedirect (statusCode: number): boolean
 }
 
 export = nettime
